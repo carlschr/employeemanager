86b4dffeb7c0affc701840803abc439df85fb4b1
carlschr <cjcarlson136@gmail.com>
2021-01-02 17:15:52 -0600

    Added manager search

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ const runApp = () => {
         choices: ['View All Departments', 'View All Employees', 'View Managers']
     })
     .then(answer => {
+        //Decides which function to run based on input
         switch (answer.actions) {
             case 'View All Departments':
                 allDepts();
@@ -41,7 +42,9 @@ const runApp = () => {
                 allEmployees();
                 break;
             case 'View Managers':
+                allManagers();
                 break;
+            //If something goes run it reruns the app
             default:
                 runApp();
                 break;
@@ -49,6 +52,7 @@ const runApp = () => {
     });
 };
 
+//Function to retrieve a list of departments
 const allDepts = () => {
     connection.query('SELECT * FROM department', (err, res) => {
         if (err) {
@@ -60,14 +64,18 @@ const allDepts = () => {
     });
 };
 
+//Function to retrieve a list of all employees
 const allEmployees = () => {
+    //Query selects columns from all three tables and orders by last name
     connection.query('SELECT employee.id, first_name, last_name, title, department_name, salary, manager_id FROM employee LEFT JOIN role ON role_id = role.id INNER JOIN department ON department.id = department_id ORDER BY employee.last_name', (err, res) => {
         if (err) {
             console.error(err);
             runApp();
         };
+        //Two loops compare the employees and pull the matching manager name for each manager_id
         res.forEach(employee => {
             res.forEach(otherEmployee => {
+                //If the manager_id is already deleted it moves to the next employee
                 if (employee.hasOwnProperty('manager_id')) {
                     if (employee.manager_id === otherEmployee.id) {
                         employee.manager = `${otherEmployee.first_name} ${otherEmployee.last_name}`;
@@ -80,6 +88,16 @@ const allEmployees = () => {
             });
         });
         console.log('\n');
+        console.table(res);
+    });
+};
+
+const allManagers = () => {
+    connection.query('SELECT employee.id, first_name, last_name, title, salary FROM employee LEFT JOIN role ON role_id = role.id WHERE manager_id = 0', (err, res) => {
+        if (err) {
+            console.error(err);
+            runApp();
+        };
         console.table(res);
     });
 };
