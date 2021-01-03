f4eadee1d7fbe3e8a9ad85fa69d86413d3a62bd1
carlschr <cjcarlson136@gmail.com>
2021-01-02 19:21:45 -0600

    Changed error response

--- routes/view.js
+++ routes/view.js
@@ -1,10 +1,12 @@
+const cTable = require('console.table');
 module.exports = {
     //Function to retrieve a list of departments
     allDepts(connection, cb){
         connection.query('SELECT * FROM department', (err, res) => {
             if (err) {
-                console.error(err);
+                console.log(`Something went wrong: ${err.message}`);
                 cb();
+                return;
             };
             console.log('\n');
             console.table(res);
@@ -16,8 +18,9 @@ module.exports = {
     allRoles(connection, cb) {
         connection.query('SELECT * FROM role', (err, res) => {
             if (err) {
-                console.error(err);
+                console.log(`Something went wrong: ${err.message}`);
                 cb();
+                return;
             };
             console.table(res);
             cb();
@@ -29,8 +32,9 @@ module.exports = {
         //Query selects columns from all three tables and orders by last name
         connection.query('SELECT employee.id, first_name, last_name, title, department_name, salary, manager_id FROM employee LEFT JOIN role ON role_id = role.id INNER JOIN department ON department.id = department_id ORDER BY employee.last_name', (err, res) => {
             if (err) {
-                console.error(err);
+                console.log(`Something went wrong: ${err.message}`);
                 cb();
+                return;
             };
             //Two loops compare the employees and pull the matching manager name for each manager_id
             res.forEach(employee => {
@@ -56,8 +60,9 @@ module.exports = {
     allManagers(connection, cb) {
         connection.query('SELECT employee.id, first_name, last_name, title, salary FROM employee LEFT JOIN role ON role_id = role.id WHERE manager_id = 0', (err, res) => {
             if (err) {
-                console.error(err);
+                console.log(`Something went wrong: ${err.message}`);
                 cb();
+                return;
             };
             console.table(res);
             cb();
